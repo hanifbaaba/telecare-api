4e6104d376b7149f6b892cdf4b5bcdfe6180125c
hanifubaaba <baabahanif88@gmail.com>
2025-08-16 03:26:59 +0100

    Created models and admin interface for Users

--- telecare/users/models.py
+++ telecare/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import  AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        PATIENT = "PATIENT", _("Patient")
+        DOCTOR = "DOCTOR", _("Doctor")
+        ADMIN = "ADMIN", _("Admin")
+
+    email = models.EmailField(_('email address'), unique = True)
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.PATIENT
+    )
+    is_verified = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.email
