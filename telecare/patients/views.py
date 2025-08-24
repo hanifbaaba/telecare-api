af5a2f412cf3ed3a59d3aadf2fd5ada671b01a24
hanifubaaba <baabahanif88@gmail.com>
2025-08-25 00:48:55 +0100

    Created Views for querying patients

--- telecare/patients/views.py
+++ telecare/patients/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Patients
+from .serializers import PatientSerializer
 
-# Create your views here.
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patients.objects.all()
+    serializer_class = PatientSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
